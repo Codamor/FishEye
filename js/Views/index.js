9ce3ed9d32b8f165f5e8da48cee943436748eac2
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-07-12 08:32:08 +0200

    refactor some class names

--- js/Views/index.js
+++ js/Views/index.js
@@ -1,46 +1,46 @@
-let db = getData() ;
-
-function displayAllPhotographersCards(event){
-
-    gallery.innerHTML = "" ;
-
-    if(event === undefined){
-
-        db
-            .then(data => {
-                let photographers = data.photographers ;
-                photographers.forEach(photographer => {
-                    gallery.innerHTML += photographerToCard(photographer) ;
-                })
-            })
-            .then(()=>{
-
-                addTagEventListener() ;
-
-            })
-    } else {
-
-        let tag = event.target.innerText.slice(1).toLowerCase() ;
-
-        db
-            .then(data => {
-                let photographers = data.photographers ;
-                for (let i = 0; i < photographers.length; i ++){
-                    for (let j = 0; j < photographers[i].tags.length; j ++){
-                        if(tag == photographers[i].tags[j]){
-                            gallery.innerHTML += photographerToCard(photographers[i]) ;
-                        }
-                    }
-                }
-            })
-            .then(()=>{
-
-                addTagEventListener() ;
-
-            })
-    }
-
+function createPhotographerTagsHtml(tags){
+    let photographerHtmlTags = "" ;
+    tags.forEach(tag => {
+        photographerHtmlTags +=
+            "<a class=\"navigation__link tag navigation__link--inCard\" title=\"Afficher les photographes de l'évenementiel\"" +
+            " role=\"link\" aria-label=\"Afficher les photographes de l'art\">" +
+            "#"+tag + "</a>\n"
+    })
+    return photographerHtmlTags ;
 }
 
-displayAllPhotographersCards() ;
+function createPhotographerHtml(photographer){
+
+    let name = photographer.name ;
+    let firstName = photographer.name.split(" ")[0] ;
+    let lastName = photographer.name.split(" ")[1] ;
+    let photographerTags = photographer.tags ;
+    let htmlTags = createPhotographerTagsHtml(photographerTags) ;
+    let portrait = firstName.replace(/-/, "").concat(lastName.replace(/-/, "")) //remove "-" for composed names
+
+    return "" +
+        "<div class=\"card\" aria-label=\"photographe\">\n" +
+        "                    <a class=\"card__link\" href=\"common/photographer-"+ firstName.toLowerCase() + "-" + lastName.toLowerCase() + ".html\" title=\"Découvrez " + name + "\" role=\"link\" aria-label=\"Découvrez " + name + "\">\n" +
+        "                        <img class=\"card__picture\" src=\"assets/media/Photographers%20ID%20Photos/"+ portrait + ".jpg\" alt=\"" + name + "\">\n" +
+        "                        <h2 class=\"card__name\">\n" +
+        "                            "+ name + "\n" +
+        "                        </h2>\n" +
+        "                    </a>\n" +
+        "                    <h3 class=\"card__location\">\n" +
+        "                        "+ photographer.city + ", "+ photographer.country + "\n" +
+        "                    </h3>\n" +
+        "                    <p class=\"card__tagline\">\n" +
+        "                        " + photographer.tagline + "\n" +
+        "                    </p>\n" +
+        "                    <p class=\"card__price\">\n" +
+        "                        " + photographer.price + "€/jour\n" +
+        "                    </p>\n" +
+
+        "                    <nav class=\"navigation navigation--inCard\" role=\"link\" aria-label=\"photographer categories\">\n" +
+        "                       "+ htmlTags +"    " +
+        "                    </nav>\n" +
+        "                </div><!-- end photographer -->"
+}
+
+
 
--- js/elements.js
+++ js/elements.js
@@ -1,14 +1,11 @@
 //DOM elements
 let gallery = document.getElementById("gallery") ;
 
-
-
-
 //EVENT listeners
 function addTagEventListener(){
     let allTags = document.getElementsByClassName("tag") ;
 
     for (let i = 0; i < allTags.length; i++){
-        allTags[i].addEventListener("click", displayAllPhotographersCards) ;
+        allTags[i].addEventListener("click", displayPhotographers) ;
     }
 }

66c6e19f6df602deb3681a7da1fa7bd8bef995e7
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-07-28 17:03:02 +0100

    add HomePage class with displayPhotographersGallery method

--- src/js/Views/HomePage.js
+++ src/js/Views/HomePage.js
@@ -1,12 +1,28 @@
-import {PhotographerController} from "/src/js/Controller/PhotographerController.js";
+import {PhotographerController} from "../Controller/PhotographerController.js";
+import {Photographer} from "../Entity/Photographer.js";
+import {addPhotographerCardEventListener} from "../elements.js";
 
 export class HomePage {
 
-    displayHomePage(){
+    async displayPhotographersGallery(){
 
-       new PhotographerController().getAllPhotographers() ;
+        let allPhotographers = new PhotographerController().getAllPhotographers() ;
+        let photographersGallery = "";
 
+        allPhotographers
+            .then(data => {
+                data.forEach(element =>{
+                    let photographer = new Photographer(element.name, element.id, element.city, element.country, element.tags, element.tagline,
+                                                        element.price, element.portrait) ;
+
+                    photographersGallery += photographer.toHtmlCard() ;
+
+                })
+                document.getElementById("gallery").innerHTML = photographersGallery ;
+            })
+            .then(addPhotographerCardEventListener)
     }
 }
+
 
 

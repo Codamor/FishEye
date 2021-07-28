da5a5864e36afa0b2ab89ce2650dca0a2cf96005
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-07-28 11:17:35 +0100

    add photographer toHtmlCard method

--- src/js/Entity/photographer.js
+++ src/js/Entity/photographer.js
@@ -1,4 +1,4 @@
-class photographer{
+export class Photographer{
     constructor(name, id, city, country, tags, tagline, price, portrait){
         this._name = name;
         this._id = id;
@@ -72,5 +72,50 @@ class photographer{
 
     set portrait(value) {
         this._portrait = value;
+    }
+
+    toHtmlCard(){
+        let htmlCard = "" ;
+        let name = this._name ;
+        let firstName = name.split(" ")[0] ;
+        let lastName = name.split(" ")[1] ;
+        let portraitNamePath = firstName.replace(/-/, "").concat(lastName.replace(/-/, "")) //remove "-" for composed first names
+        let photographerTags = this._tags ;
+
+        let photographerHtmlTags = "" ;
+        photographerTags.forEach(tag => {
+            photographerHtmlTags +=
+                "<a class=\"navigation__link tag navigation__link--inCard\" title=\"Afficher les photographes de l'évenementiel\"" +
+                " role=\"link\" aria-label=\"Afficher les photographes de l'art\">" +
+                "#"+tag + "</a>\n"
+        })
+
+        return htmlCard +=
+
+        `<div class="card" aria-label="photographe">
+            <a class="card__link" href="common/photographer-${firstName.toLowerCase()}-${lastName.toLowerCase()}.html" title="Découvrez ${firstName}"  role="link" aria-label="Découvrez ${firstName}" >
+                 <img class="card__picture" src="assets/media/Photographers%20ID%20Photos/${portraitNamePath}.jpg" alt="">
+               <h2 class="card__name">
+                 ${name}
+               </h2>
+            </a>
+            <h3 class="cards__location">
+                 ${this._city}, ${this._country}
+            </h3>
+            <p class="card__tagline">
+                 ${this._tagline}
+            </p>
+            <p class="card__price">
+                 ${this._price} €/jour
+            </p>
+        
+           <nav class="navigation navigation--inCard" role="link" aria-label="photographer categories">
+                ${photographerHtmlTags}
+           </nav>
+        </div><!-- end photographer -->`
+
+
+
+
     }
 }

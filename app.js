99a5082c0b06cdf6e1a66580c791c36b0044e79f
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-07-28 13:57:57 +0100

    add isHomePage, isPhotographerPage functions and displayPageContent function declaration

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-import {FishEyeApi} from "./src/api/FishEye.js" ;
+
+import {PhotographerController} from "./src/js/Controller/photographerController.js";
+import {MediaController} from "./src/js/Controller/mediaController.js";
+
+
+function isHomePage(){
+    return window.location.href.includes("index") ;
+}
+
+function isPhotographerPage(){
+    return window.location.href.includes("photographer") ;
+}
+
+function displayPageContent(){
+
+    if (isHomePage()){
+
+    }
+
+    else if (isPhotographerPage()){
+
+    }
+}
+
+displayPageContent()
 
 
 

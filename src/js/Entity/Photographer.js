d8ba07384448b471de46814b7c5d5409f84f58b9
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-07-30 15:05:46 +0100

    add photographer ID to <a> link element

--- src/js/Entity/Photographer.js
+++ src/js/Entity/Photographer.js
@@ -96,7 +96,7 @@ export class Photographer{
         return htmlCard +=
 
         `<div class="card" visible="true" aria-label="photographe">
-            <a class="card__link" href="common/photographer-${firstName.toLowerCase()}-${lastName.toLowerCase()}.html" title="Découvrez ${firstName}"  role="link" aria-label="Découvrez ${firstName}" >
+            <a id= "${this._id}" class="card__link" href="common/photographer.html" title="Découvrez ${firstName}"  role="link" aria-label="Découvrez ${firstName}" >
                  <img class="card__picture" src="public/media/Photographers%20ID%20Photos/${portraitNamePath}.jpg" alt="">
                <h2 class="card__name">
                  ${name}

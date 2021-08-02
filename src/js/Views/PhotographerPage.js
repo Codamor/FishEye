fcdc891901dccfe8d946c371a7d3e8e8cdd85cf2
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-02 11:01:31 +0100

    add display photographer media list feature

--- src/js/Views/PhotographerPage.js
+++ src/js/Views/PhotographerPage.js
@@ -1,5 +1,7 @@
 import {Photographer} from "../Entity/Photographer.js";
 import {PhotographerController} from "../Controller/PhotographerController.js";
+import {MediaController} from "../Controller/MediaController.js";
+import {MediaFactory} from "../Factory/MediaFactory.js";
 
 export class PhotographerPage{
 
@@ -9,23 +11,44 @@ export class PhotographerPage{
 
         let photographerId = photographerPageUrl.searchParams.get("id") ;
 
-        let allPhotographer = new PhotographerController().getAllPhotographers() ;
+        let allPhotographers = new PhotographerController().getAllPhotographersList() ; //TODO replace by getPhotographerMediaList
 
-        allPhotographer.then(photographers => {
+        let thisPhotographerMedia = new MediaController().getPhotographerMediaList(photographerId) ;
+
+        let mediaFactory = new MediaFactory() ;
+
+        let htmlGallery = "" ;
+
+        allPhotographers.then(photographers => {
             photographers.forEach(element => {
                 if(element.id == photographerId){
 
                     //create photographer
-                    let photographer = new Photographer(element.name, element.id, element.city, element.country,
-                                                        element.tags, element.tagline, element.price, element.portrait) ;
+                    let photographer = new Photographer(element.name,
+                                                        element.id,
+                                                        element.city,
+                                                        element.country,
+                                                        element.tags,
+                                                        element.tagline,
+                                                        element.price,
+                                                        element.portrait) ;
 
                     //display photographer meta information
                     photographer.toHtmlMetaInformations() ;
 
-                    //create photographer banner
+                    //display photographer banner
                     photographer.toHtmlBanner() ;
 
-                    //create photographer gallery
+                    //display photographer media list
+                    thisPhotographerMedia.then(allMedia => {
+                        allMedia.forEach(element => {
+                            let media = mediaFactory.createMedia(element) ;
+
+                            htmlGallery += media.toHtml(photographer.name.split(" ")[0])
+
+                        })
+                        document.getElementById("gallery").innerHTML += htmlGallery ;
+                    })
                 }
             })
         })

48deb7cd8d0561d0fa2d60af20b8941cbd3b0816
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-02 09:24:58 +0100

    photographer object creation in controller

--- src/js/Controller/PhotographerController.js
+++ src/js/Controller/PhotographerController.js
@@ -1,23 +1,49 @@
 import {FishEyeApi} from "/src/api/FishEye.js";
+import {Photographer} from "../Entity/Photographer.js";
 
 export class PhotographerController {
 
-    async getAllPhotographers(){
+    async getAllPhotographersList(){
+
+        let allPhotographersList = [] ;
+
         const FISHEYEDATA = await new FishEyeApi().getFishEyeData() ;
 
-        return FISHEYEDATA.photographers ;
+        FISHEYEDATA.photographers.forEach(element => {
+            let photographer = new Photographer(element.name,
+                                                element.id,
+                                                element.city,
+                                                element.country,
+                                                element.tags,
+                                                element.tagline,
+                                                element.price,
+                                                element.portrait) ;
 
+            allPhotographersList.push(photographer) ;
+
+        }) ;
+
+        return allPhotographersList ;
     }
 
     async getPhotographerById(photographerId){
 
         const FISHEYEDATA = await new FishEyeApi().getFishEyeData() ;
 
-        let allPhotographers = FISHEYEDATA.photographers ;
+        FISHEYEDATA.photographers.forEach(element => {
+            if(element.id == photographerId){
+                let photographer = new Photographer(element.name,
+                    element.id,
+                    element.city,
+                    element.country,
+                    element.tags,
+                    element.tagline,
+                    element.price,
+                    element.portrait) ;
 
-        //TODO why allPhotographers.then not a function
-
-
+                return photographer ;
+            }
+        }) ;
     }
 }
 

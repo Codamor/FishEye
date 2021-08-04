a1bd7eebaaf00e1fc3dd9ebf3a44ab817dd0af16
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-04 17:54:38 +0100

    add Events class controller to addEventListeners

---
+++ src/js/Controllers/Events.js
@@ -0,0 +1,36 @@
+import {PhotographerPageView} from "../Views/PhotographerPageView.js";
+import {LightBoxView} from "../Views/lightBoxView.js";
+
+export class Events{
+    constructor() {
+    }
+
+    addEventListenerOnMediaToOpenLightBox(){
+        let lightBoxView = new LightBoxView() ;
+        let allMedia = document.getElementsByClassName("media__element") ;
+
+        for (let i = 0; i < allMedia.length; i++) {
+            allMedia[i].addEventListener("click", (lightBoxView.openLightBox))
+        }
+    }
+
+    addEventListenerOnLightBoxCloseButton(){
+        let lightBoxView = new LightBoxView() ;
+        let lightBoxCloseButton = document.getElementById("close") ;
+
+        lightBoxCloseButton
+            .addEventListener("click", lightBoxView.closeMediaLightBox)
+    }
+
+    addEventListenerOnLightBoxPreviousButton(){
+        document
+            .getElementById("prev")
+            .addEventListener("click", lightBoxView.previousMedia) ;
+    }
+
+    addEventListenerOnLightBoxPreviousButton(){
+        document
+            .getElementById("prev")
+            .addEventListener("click", lightBoxView.nextMedia) ;
+    }
+}

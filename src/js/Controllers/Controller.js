c406eae287c1a1641026e1f5fc2b9ddf9c0aad77
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-04 17:58:09 +0100

    add eventListeners to photographerPage elements creation loop

--- src/js/Controllers/Controller.js
+++ src/js/Controllers/Controller.js
@@ -1,6 +1,7 @@
 import {FishEyeApi} from "../Models/FishEye.js";
 import {HomePageView} from "../Views/HomePageView.js";
 import {PhotographerPageView} from "../Views/PhotographerPageView.js";
+import {LightBoxView} from "../Views/LightBoxView.js";
 import {Events} from "./Events.js";
 
 
@@ -40,6 +41,7 @@ export class Controller{
 
             let fishEyeApi = new FishEyeApi("/src/api/FishEye.json") ;
             let photographerPageView = new PhotographerPageView() ;
+            let lightBoxView = new LightBoxView() ;
             let events = new Events() ;
 
             let photographerPageId = this.getPhotographerPageId() ;
@@ -58,6 +60,7 @@ export class Controller{
                     allPhotographerMedia
                         .then(allMedia => {
                             photographerPageView.toHtmlGallery(allMedia, photographerName) ;
+                            lightBoxView.toHtmlLightBoxGallery(allMedia, photographerName) ;
                         })
                         .then(events.addEventListenerOnMediaToOpenLightBox) ;
                 })

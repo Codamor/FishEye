766ae8e7d3de3447e468d073f77d9280b71a7b12
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-10 15:42:52 +0100

    add getAllPhotographersByCategory method

--- src/js/Models/Model.js
+++ src/js/Models/Model.js
@@ -28,5 +28,52 @@ export class Model {
         return  allPhotographers ;
     }
 
-    
+    async getPhotographerById(photographerId) {
+
+        let response = await fetch(this._db);
+        let data = await response.json();
+
+        let photographer ;
+
+        data.photographers.forEach(element => {
+
+            if(element.id === photographerId){
+                photographer = new Photographer( element.name,
+                    element.id,
+                    element.city,
+                    element.country,
+                    element.tags,
+                    element.tagline,
+                    element.price,
+                    element.portrait) ;
+            }
+        });
+        return photographer ;
+    }
+
+    async getAllPhotographersByCategory(category) {
+
+        let response = await fetch(this._db);
+        let data = await response.json();
+
+        let allPhotographersByCategoryArray = [] ;
+
+        data.photographers.forEach(photographerInPhotographersData => {
+            photographerInPhotographersData.tags.forEach(tag => {
+                if (category === tag){
+                    let photographer = new Photographer(photographerInPhotographersData.name,
+                        photographerInPhotographersData.id,
+                        photographerInPhotographersData.city,
+                        photographerInPhotographersData.country,
+                        photographerInPhotographersData.tags,
+                        photographerInPhotographersData.tagline,
+                        photographerInPhotographersData.price,
+                        photographerInPhotographersData.portrait) ;
+
+                    allPhotographersByCategoryArray.push(photographer) ;
+                }
+            })
+        });
+        return allPhotographersByCategoryArray ;
+    }
 }

ce437adfc1849cb01fe7f64182cacbda421bada4
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-10 16:03:52 +0100

    add app const

---
+++ app.js
@@ -0,0 +1,8 @@
+import {Controller} from "./src/js/Controllers/Controller.js";
+import {Model} from "./src/js/Models/Model.js";
+import {View} from "./src/js/Views/View.js";
+
+const app = new Controller(new Model("src/api/FishEye.json"), new View()) ;
+
+
+

005a4f0f4cbc614a7dd6bd69104b6e1bc77c18b4
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-10 17:16:27 +0100

    add createPhotographerBanner method

--- src/js/Views/View.js
+++ src/js/Views/View.js
@@ -48,14 +48,17 @@ export class View {
     }
 
     createPhotographerMetaData(photographer){
-        document.title = `${photographer._name}, photographe spécialiste.` ;//TODO add tags
-        document.querySelector('meta[name="description"]').setAttribute("content", `${photographer._name}, photographe spécialiste.`);
-    }
-
-    createPhotographerBanner(photographer){
-
         photographer
             .then(element => {
+                document.title = `${element._name}, photographe spécialiste.` ;//TODO add tags
+                document.querySelector('meta[name="description"]').setAttribute("content", `${element._name}, photographe spécialiste.`);
+            })
+        }
+
+    createPhotographerBanner(photographer){
+        photographer
+            .then(element => {
+                console.log(element)
                 let portraitPicturePath = `../media/Photographers%20ID%20Photos/${element.portrait}`
                 let photographerTags = element.tags ;
 
@@ -72,32 +75,82 @@ export class View {
 
                 let htmlBanner =
                     `<div class="about__informations">
-                <div class="card card--photographer-page" aria-label="photographe">
-                    <h1 id="card__name" class="card__name card__name--photographer-page">
-                        ${element.name}
-                    </h1>
-                    <h2 class="card__location card__location--photographer-page">
-                        ${element.city}, ${element.country}
-                    </h2>
-                    <p class="card__tagline card__tagline--photographer--page">
-                        ${element.tagline}
-                    </p>
-                    <nav class="navigation navigation--photographerPage" role="link" aria-label="photographer categories">
-                        ${photographerHtmlTags}
-                    </nav>
+                        <div class="card card--photographer-page" aria-label="photographe">
+                            <h1 id="card__name" class="card__name card__name--photographer-page">
+                                ${element.name}
+                            </h1>
+                            <h2 class="card__location card__location--photographer-page">
+                                ${element.city}, ${element.country}
+                            </h2>
+                            <p class="card__tagline card__tagline--photographer--page">
+                                ${element.tagline}
+                            </p>
+                            <nav class="navigation navigation--photographerPage" role="link" aria-label="photographer categories">
+                                ${photographerHtmlTags}
+                            </nav>
+                        </div><!-- end about informations -->
 
-                </div><!-- end photographer -->
+                        <button id="contact" class="button button--contact">Contactez-moi</button>
 
-                <button id="contact" class="button button--contact">Contactez-moi</button>
-
-            </div>
-
-            <div class="bio__picture">
-                <img class="card__picture card__picture--photographer-page" src="${portraitPicturePath}" alt="${element._name}">
-            </div>`
+                    </div>
+    
+                    <div class="bio__picture">
+                        <img class="card__picture card__picture--photographer-page" src="${portraitPicturePath}" alt="${element._name}">
+                    </div>`
 
                 document.getElementById("about").innerHTML += htmlBanner ;
             }) ;
     }
 
+    createMediaGallery(photographerMedia){
+
+        let htmlGallery = "" ;
+
+        //get only first name and remove "-" for composed first names
+        let photographerNameForMediaPath = photographerName.split(" ")[0].replace("-"," ") ;
+
+        photographerMedia.forEach(media => {
+
+            if (media.image){
+
+                let mediaPath = `/public/media/${photographerNameForMediaPath}/${media.image}`
+
+                htmlGallery +=
+                    `   <div class="media" media-category="${media.tags}" visible="true">
+                            <picture class="media__element">
+                                <img id="${media.id}" src="${mediaPath}" date="${media.date}"alt="super architecture"> 
+                            </picture>
+                            <div class="media__informations">
+                                <div class="media__title">
+                                    ${media._title}
+                                </div>
+                                <div class="media__likes">
+                                    ${media._likes}
+                                </div>
+                            </div>
+                        </div>`
+
+            } else if (media.video){
+
+                let mediaPath = `/public/media/${photographerNameForMediaPath}/${media.video}`
+
+                htmlGallery +=
+                    `   <div class="media" media-category="${media.tags}" visible="true">
+                            <video class="media__element">
+                                <source id="${media.id}" src="${mediaPath}" date="${media.date}" alt="super architecture"> 
+                            </video>
+                            <div class="media__informations">
+                                <div class="media__title">
+                                    ${media._title}
+                                </div>
+                                <div class="media__likes">
+                                    ${media._likes}
+                                </div>
+                            </div>
+                        </div>`
+            }
+
+        })
+        document.getElementById("media-gallery").innerHTML += htmlGallery ;
+    }
 }

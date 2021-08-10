961232a2633580821e3c8400b125690f86a944f4
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-10 15:41:22 +0100

    add getAllPhotographers method

--- src/js/Models/Model.js
+++ src/js/Models/Model.js
@@ -1,8 +1,31 @@
-export class FishEyeModel {
-    constructor() {
+import {Photographer} from "./Photographer.js";
+import {Media} from "./Media.js";
+
+
+export class Model {
+    constructor(db) {
+        this._db = db ;
     }
 
-    getData(){
-        console.log("data")
+    async getAllPhotographers() {
+
+        let response = await fetch(this._db);
+        let data = await response.json();
+
+        let allPhotographers = [];
+
+        await data.photographers.forEach(element => {
+            allPhotographers.push(new Photographer( element.name,
+                element.id,
+                element.city,
+                element.country,
+                element.tags,
+                element.tagline,
+                element.price,
+                element.portrait));
+        });
+
+        return  allPhotographers ;
     }
+
 }

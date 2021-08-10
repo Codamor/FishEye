7414eff980a3262653318ad5e01ff488ad5cd08d
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-10 17:11:11 +0100

    add createPhotographerMetaInformations and createPhotographerBanner methods

--- src/js/Views/View.js
+++ src/js/Views/View.js
@@ -47,5 +47,57 @@ export class View {
             })
     }
 
-    
+    createPhotographerMetaData(photographer){
+        document.title = `${photographer._name}, photographe spécialiste.` ;//TODO add tags
+        document.querySelector('meta[name="description"]').setAttribute("content", `${photographer._name}, photographe spécialiste.`);
+    }
+
+    createPhotographerBanner(photographer){
+
+        photographer
+            .then(element => {
+                let portraitPicturePath = `../media/Photographers%20ID%20Photos/${element.portrait}`
+                let photographerTags = element.tags ;
+
+                let photographerHtmlTags = "" ;
+                photographerTags.forEach(tag => {
+                    photographerHtmlTags +=
+                        `<a class="navigation__link navigation__link--inCard tag" enabled="false" title="Afficher les photographies de la catégorie ${tag}"
+                    role="link" aria-label="Afficher les photographies de la catégorie ${tag}">
+                    
+                    #${tag}
+                   
+                 </a>`
+                }) ;
+
+                let htmlBanner =
+                    `<div class="about__informations">
+                <div class="card card--photographer-page" aria-label="photographe">
+                    <h1 id="card__name" class="card__name card__name--photographer-page">
+                        ${element.name}
+                    </h1>
+                    <h2 class="card__location card__location--photographer-page">
+                        ${element.city}, ${element.country}
+                    </h2>
+                    <p class="card__tagline card__tagline--photographer--page">
+                        ${element.tagline}
+                    </p>
+                    <nav class="navigation navigation--photographerPage" role="link" aria-label="photographer categories">
+                        ${photographerHtmlTags}
+                    </nav>
+
+                </div><!-- end photographer -->
+
+                <button id="contact" class="button button--contact">Contactez-moi</button>
+
+            </div>
+
+            <div class="bio__picture">
+                <img class="card__picture card__picture--photographer-page" src="${portraitPicturePath}" alt="${element._name}">
+            </div>`
+
+                document.getElementById("about").innerHTML += htmlBanner ;
+            }) ;
+    }
+
 }

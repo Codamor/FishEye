024dcdc740535cd06114e4fc41c4cf750a0e42aa
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-16 14:41:25 +0100

    change media path for lightbox and photographer picture

--- src/js/Views/View.js
+++ src/js/Views/View.js
@@ -156,7 +156,7 @@ export class View {
 
                             if (media.image){
 
-                                let mediaPath = `./pubdlic/media/${photographerNameForMediaPath}/${media.image}`
+                                let mediaPath = `./public/media/${photographerNameForMediaPath}/${media.image}`
 
                                 mediaGallery += //TODO solve mediaId at 2 locations
                                     `<div class="media" data-media-id="${media.id}" data-media-type="picture"  data-media-status="default" data-media-title="${media.title}" data-media-category="${media.tags}" data-media-date="${media.date}" data-media-likes="${media.likes}">
